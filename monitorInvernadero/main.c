4337fd03b73408889c4f2d44eb8cc1051b253c15
Jeremias Labella <labellajeremias@gmail.com>
2026-06-23 14:24:14 -0300

    first commit

---
+++ monitorInvernadero/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 6/23/2026 11:39:27 AM
+ *  Author: labej
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
